5301cbd867532d7cf8b46003f74f25281ddfad7b
Om Gandhi <omgandhi255@gmail.com>
2022-08-15 15:53:56 +0530

    Work on the UI and betterment of it

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+import androidx.core.widget.ContentLoadingProgressBar;
 
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
@@ -28,86 +36,107 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-TextView tempinfo,locinfo,descinfo;
-ImageView iconview;
+    TextView tempinfo, locinfo, descinfo,maininfo,pres,humi,maxtemp,mintemp,feels_like;
+    ImageView iconview;
+    ConstraintLayout layout;
+    LinearLayout ll1;
+
+
     private GpsTracker gpsTracker;
 
     @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+
 
         if (android.os.Build.VERSION.SDK_INT > 9) {
             StrictMode.ThreadPolicy gfgPolicy =
                     new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(gfgPolicy);
         }
-        tempinfo=(TextView)findViewById(R.id.tempinfo);
-        descinfo=(TextView)findViewById(R.id.descinfo);
-        locinfo=(TextView)findViewById(R.id.locinfo);
-        iconview=(ImageView)findViewById(R.id.iconview);
+        tempinfo = (TextView) findViewById(R.id.tempinfo);
+        maininfo = (TextView) findViewById(R.id.maininfo);
+        descinfo = (TextView) findViewById(R.id.descinfo);
+        locinfo = (TextView) findViewById(R.id.locinfo);
+        pres = (TextView) findViewById(R.id.pres);
+        humi = (TextView) findViewById(R.id.humi);
+        maxtemp = (TextView) findViewById(R.id.maxtemp);
+        mintemp = (TextView) findViewById(R.id.mintemp);
+        feels_like = (TextView) findViewById(R.id.feels_like);
+        layout=(ConstraintLayout)findViewById(R.id.layout);
+        ll1=(LinearLayout)findViewById(R.id.ll1);
+        iconview = (ImageView) findViewById(R.id.iconview);
+
         try {
-            if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-    
+
         Geocoder geocoder;
         List<Address> addresses;
         String city = "";
-        String main="";
-        String desc="";
-        double latitude=0 ;
+        String main = "";
+        String desc = "";
+        double latitude = 0;
         double longitude = 0;
 //        21.870640,73.504288
-        latitude=getLat(latitude);
-        longitude=getLon(longitude);
+        latitude = getLat(latitude);
+        longitude = getLon(longitude);
         System.out.println(latitude);
         System.out.println(longitude);
         geocoder = new Geocoder(this, Locale.getDefault());
-        String icon="";
-        String iconUrl="https://openweathermap.org/img/wn/";
+        String icon = "";
+        String iconUrl = "https://openweathermap.org/img/wn/";
 
+
+        int temp = 0;
+        int min_temp=0,max_temp=0,humid=0,feelslike=0;
+        float pressure = 0;
+        String accuweather = "https://dataservice.accuweather.com//forecasts/v1/daily/1day/197790?apikey=hwDC6Dttgzg8bmVTYaX4Tg80t1Jb3UG7&details=true&metric=true";
+        String openweatherapikey = "a9a7d60c7fb542a91b03a3c864b5b11b";
+
+
+        OkHttpClient client = new OkHttpClient();
         try {
             addresses = geocoder.getFromLocation(latitude, longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
-             // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+            // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
             city = addresses.get(0).getLocality();
+            Request request = new Request.Builder()
+                    .url("https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=a9a7d60c7fb542a91b03a3c864b5b11b&units=metric")
+                    .get()
+                    .build();
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        locinfo.setText(city);
-        int temp = 0;
+            String min = "";
 
-        String accuweather="https://dataservice.accuweather.com//forecasts/v1/daily/1day/197790?apikey=hwDC6Dttgzg8bmVTYaX4Tg80t1Jb3UG7&details=true&metric=true";
-        String openweatherapikey="a9a7d60c7fb542a91b03a3c864b5b11b";
-        OkHttpClient client = new OkHttpClient();
-        try {Request request = new Request.Builder()
-                        .url("https://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid=a9a7d60c7fb542a91b03a3c864b5b11b&units=metric")
-                        .get()
-                        .build();
-
-                    String min="";
-
-                    Response response = client.newCall(request).execute();
-                    // Your network activity
-                    String jsonData =response.body().string();
-                    System.out.println(jsonData);
-                    JSONObject Jobj1=new JSONObject(jsonData);
-                    JSONObject temperature=Jobj1.getJSONObject("main");
-                    temp=temperature.getInt("temp");
-                    System.out.println(temp);
+            Response response = client.newCall(request).execute();
+            // Your network activity
+            String jsonData = response.body().string();
+            System.out.println(jsonData);
+            JSONObject Jobj1 = new JSONObject(jsonData);
+            JSONObject temperature = Jobj1.getJSONObject("main");
+            temp = temperature.getInt("temp");
+            min_temp = temperature.getInt("temp_min");
+            max_temp = temperature.getInt("temp_max");
+            feelslike = temperature.getInt("feels_like");
+            humid = temperature.getInt("humidity");
+            pressure = (float) temperature.getDouble("pressure");
+            System.out.println(temp);
             JSONArray jsonArray = Jobj1.getJSONArray("weather");
-                    for (int i = 0; i < jsonArray.length(); i++) {
-                        icon = jsonArray.getJSONObject(i).getString("icon");
-                        main=jsonArray.getJSONObject(i).getString("main");
-                        desc=jsonArray.getJSONObject(i).getString("description");
-                    }
+            for (int i = 0; i < jsonArray.length(); i++) {
+                icon = jsonArray.getJSONObject(i).getString("icon");
+                main = jsonArray.getJSONObject(i).getString("main");
+                desc = jsonArray.getJSONObject(i).getString("description");
+            }
 
-                    System.out.println(icon);
+            System.out.println(icon);
+
 
 //                    JSONObject Jobject = new JSONObject(jsonData);
 //                    JSONArray jsonArray = Jobject.getJSONArray("DailyForecasts");
@@ -122,36 +151,83 @@ ImageView iconview;
 //                    System.out.println(value);
 //                    System.out.println(unit);
 ////                    System.out.println(Jobject.getClass().getSimpleName());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if(main.equals("Rain"))
+        {
+            layout.setBackgroundColor(Color.parseColor("#401b4965"));
+            ll1.setBackgroundColor(Color.parseColor("#1b4965"));
+            locinfo.setTextColor(Color.parseColor("#6c757d"));
+            descinfo.setTextColor(Color.parseColor("#6c757d"));
+            maininfo.setTextColor(Color.parseColor("#6c757d"));
+            tempinfo.setTextColor(Color.parseColor("#6c757d"));
+        }
+        else if(main.equals("Clouds"))
+        {
+            layout.setBackgroundColor(Color.parseColor("#62b6cb"));
+            ll1.setBackgroundColor(Color.parseColor("#005f73"));
+            locinfo.setTextColor(Color.parseColor("#edf6f9"));
+            descinfo.setTextColor(Color.parseColor("#edf6f9"));
+            maininfo.setTextColor(Color.parseColor("#edf6f9"));
+            tempinfo.setTextColor(Color.parseColor("#edf6f9"));
+        }
+        else if(main.equals("Snow"))
+        {
+            layout.setBackgroundColor(Color.parseColor("#40cae9ff"));
+            ll1.setBackgroundColor(Color.parseColor("#40cae9ff"));
+        }
+        else if(main.equals("Clear"))
+        {
+            layout.setBackgroundColor(Color.parseColor("#40bee9e8"));
+            ll1.setBackgroundColor(Color.parseColor("#023047"));
+            locinfo.setTextColor(Color.parseColor("#023047"));
+            descinfo.setTextColor(Color.parseColor("#023047"));
+            maininfo.setTextColor(Color.parseColor("#023047"));
+            tempinfo.setTextColor(Color.parseColor("#023047"));
+        }
+        else if(main.equals("Thunderstorm"))
+        {
+            layout.setBackgroundColor(Color.parseColor("#40003049"));
+            ll1.setBackgroundColor(Color.parseColor("#40003049"));
+        }
 
-
-            tempinfo.setText(String.valueOf(temp)+" °C");
-            descinfo.setText(desc);
-        String finalUrl=iconUrl+icon+"@4x.png";
+        locinfo.setText("\uD83D\uDCCD "+city);
+        tempinfo.setText(String.valueOf(temp) + " °C");
+        maininfo.setText(main+"y");
+        maxtemp.setText(String.valueOf(max_temp)+" °C");
+        mintemp.setText(String.valueOf(min_temp)+" °C");
+        feels_like.setText(String.valueOf(feelslike)+" °C");
+        pres.setText(String.valueOf(String.format("%.2f",pressure*0.750062))+" mmHg");
+        humi.setText(String.valueOf(humid)+" %");
+        descinfo.setText(desc);
+        String finalUrl = iconUrl + icon + "@4x.png";
         System.out.println(finalUrl);
-            Picasso.with(MainActivity.this).load(finalUrl).into(iconview);
-//
+        Picasso.with(MainActivity.this).load(finalUrl).into(iconview);
+
 
 
     }
-    public double getLat(double lat){
+
+    public double getLat(double lat) {
         gpsTracker = new GpsTracker(MainActivity.this);
-        if(gpsTracker.canGetLocation()){
+        if (gpsTracker.canGetLocation()) {
             lat = gpsTracker.getLatitude();
-        }else{
+        } else {
             gpsTracker.showSettingsAlert();
         }
         return lat;
     }
-    public double getLon(double lon){
+
+    public double getLon(double lon) {
         gpsTracker = new GpsTracker(MainActivity.this);
-        if(gpsTracker.canGetLocation()){
+        if (gpsTracker.canGetLocation()) {
             lon = gpsTracker.getLongitude();
-        }else{
+        } else {
             gpsTracker.showSettingsAlert();
         }
         return lon;
     }
+
+
 }
